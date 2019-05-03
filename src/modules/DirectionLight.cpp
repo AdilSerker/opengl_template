3b73b93a54b896171cb864300ae6838774763231
Adil <adilserkerov@gmail.com>
2019-05-03 04:20:26 +0300

    k

--- src/modules/DirectionLight.cpp
+++ src/modules/DirectionLight.cpp
@@ -1,7 +1,7 @@
 #include "DirectionLight.h"
 
 DirectionLight::DirectionLight() :
-    direction(glm::vec3(0.1f, 0.1f, 0.1f)),
+    direction(glm::vec3(0.0f, -1.0f, 0.0f)),
     ambient(glm::vec3(0.2f, 0.2f, 0.2f)),
 	diffuse(glm::vec3(0.8f, 0.8f, 0.8f)),
 	specular(glm::vec3(1.0f, 1.0f, 1.0f))
